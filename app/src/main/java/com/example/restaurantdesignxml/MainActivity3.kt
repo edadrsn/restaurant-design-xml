4d5c969cc0a27bf88ef8de8d93757167c763d250
Eda Dursun <edadrsn929@gmail.com>
2025-05-01 19:15:30 +0300

    xml view3 updated and a new function added

--- app/src/main/java/com/example/restaurantdesignxml/MainActivity3.kt
+++ app/src/main/java/com/example/restaurantdesignxml/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.restaurantdesignxml
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity3 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun goToActivity4(view: View){
+        val intent= Intent(this@MainActivity3,MainActivity4::class.java)
+        startActivity(intent)
     }
 }
